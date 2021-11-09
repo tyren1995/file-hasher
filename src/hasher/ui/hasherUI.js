8d58413d530df5784ae9dcc5eb3f2045db447fc3
Tyren Rhinehart <tyren@Tyrens-MacBook-Pro.local>
2021-11-08 22:00:18 -0500

    install dropzone, import it into ui

--- src/hasher/ui/hasherUI.js
+++ src/hasher/ui/hasherUI.js
@@ -1,10 +1,18 @@
-import '../styles/hasherStyles';
+import React, {useCallback} from 'react';
+import Dropzone from 'react-dropzone';
 
 const FileHasherUI = () => {
     return(
-        <div>
-            Drop Files Here
-        </div>
+        <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+  {({getRootProps, getInputProps}) => (
+    <section>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+    </section>
+  )}
+</Dropzone>
     )
 }
 
