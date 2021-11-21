f173eea53a29ffbf3bdbfcc09a05e46203e88125
Tyren Rhinehart <tyren@Tyrens-MacBook-Pro.local>
2021-11-21 16:57:40 -0500

    format document

--- src/hasher/ui/hasherUI.js
+++ src/hasher/ui/hasherUI.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import styles from '../styles/hasherUIStyles.module.css';
 import FileHasherLogic from '../logic/hasherLogic';
-import {useDropzone} from 'react-dropzone';
+import { useDropzone } from 'react-dropzone';
 import ClipLoader from "react-spinners/ClipLoader";
 
 const FileHasherUI = (props) => {
   const { hashFiles, deleteFile, hashedFiles, isLoading } = FileHasherLogic();
-  const {getRootProps, getInputProps} = useDropzone({onDrop:hashFiles});
-  
-  
-  const files =  hashedFiles.map((hashedFile,index) => (
+  const { getRootProps, getInputProps } = useDropzone({ onDrop: hashFiles });
+
+
+  const files = hashedFiles.map((hashedFile, index) => (
     <tr key={index} >
       <td >{hashedFile['file_name']}</td>
-      <td align="right">{hashedFile['file_size'] ? (hashedFile['file_size']+' mb') : ('NA')}</td>
+      <td align="right">{hashedFile['file_size'] ? (hashedFile['file_size'] + ' mb') : ('NA')}</td>
       <td align="right">{hashedFile['file_type'] ? (hashedFile['file_type']) : ('NA')}</td>
       <td align="right">{hashedFile['md5_hash'] ? (hashedFile['md5_hash']) : ('NA')}</td>
       <td align="right">{hashedFile['sha1-base16'] ? (hashedFile['sha1-base16']) : ('NA')}</td>
       <td align="right">{hashedFile['sha2'] ? (hashedFile['sha2']) : ('NA')}</td>
-      <td align="right" className={styles.buttonCell}><button onClick={(e)=> {e.stopPropagation(); deleteFile(hashedFile)}}><b>Delete</b></button></td>
+      <td align="right" className={styles.buttonCell}><button onClick={(e) => { e.stopPropagation(); deleteFile(hashedFile) }}><b>Delete</b></button></td>
     </tr>
   ));
 
@@ -25,15 +25,15 @@ const FileHasherUI = (props) => {
     <table>
       <thead>
         <tr>
-    <th>File Name</th>
-    <th>File Size</th>
-    <th>File Type</th>
-    <th>MD5</th>
-    <th>SHA-1</th>
-    <th>SHA-2</th>
-    <th>Actions</th>
-    
-    </tr>
+          <th>File Name</th>
+          <th>File Size</th>
+          <th>File Type</th>
+          <th>MD5</th>
+          <th>SHA-1</th>
+          <th>SHA-2</th>
+          <th>Actions</th>
+
+        </tr>
       </thead>
       <tbody>
         {files}
@@ -43,13 +43,13 @@ const FileHasherUI = (props) => {
 
   return (
     <div className={styles.container}>
-      <div {...getRootProps({className: 'dropzone'})}>
+      <div {...getRootProps({ className: 'dropzone' })}>
         <input {...getInputProps()} />
         <p>Drag 'n' drop some files here, or click to select files</p>
-      
-      <section>
-        {isLoading? (<ClipLoader/>) : (table())}
-      </section>
+
+        <section>
+          {isLoading ? (<ClipLoader />) : (table())}
+        </section>
       </div>
     </div>
   );
